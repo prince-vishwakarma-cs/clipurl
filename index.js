635190f572b11778b022fab09df5ef18d0a006ff
prince-vishwakarma-cs <pv913177@gmail.com>
2024-12-14 17:23:45 +0530

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import links from "./link.js";
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 

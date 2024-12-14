81dd6d10836836fdf4e634ddc9dde54874be791d
prince-vishwakarma-cs <pv913177@gmail.com>
2024-12-14 14:52:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+import express from "express";
+import mongoose from "mongoose";
+import links from "./link.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+mongoose
+  .connect("mongodb://localhost:27017", {
+    dbName: "link_shortener",
+  })
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { prefix: process.env.prefix });
+});
+
+app.post("/short", async (req, res) => {
+  const { link } = req.body;
+  const prefix = process.env.prefix;
+  const new_link = Math.random().toString(36).slice(2, 7);
+
+  const exist = await links.exists({ shorten_url: new_link });
+
+  if (exist)
+    return res.status(400).json({
+      success: false,
+      message: "Some error occured , please try again",
+    });
+
+  await links.create({
+    url: link,
+    shorten_url: new_link,
+  });
+
+  const new_url = prefix + new_link;
+  res.status(200).json({
+    success: true,
+    result: new_url,
+  });
+});
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const link = await links.findOne({ shorten_url: id });
+
+  if (!link)
+    return res.status(400).json({
+      success: false,
+      message: "Destination not found",
+    });
+
+  if (link.clicks_left == 0)
+    return res.status(400).json({
+      success: false,
+      message: "link is expired",
+    });
+
+  link.clicks_left -= 1;
+
+  await link.save();
+
+  res.redirect(link.url);
+});
+
+app.listen(5000, () => {
+  console.log("server is running");
+});
---
+++ link.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const schema = new Schema({
+    url:String,
+    shorten_url:String,
+    clicks_left:{type:Number,default:process.env.limit || 10}
+})
+
+const links=new mongoose.model("link",schema)
+
+export default links

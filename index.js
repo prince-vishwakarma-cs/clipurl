f03de31877de646578e76f939b16aec011559173
prince-vishwakarma-cs <pv913177@gmail.com>
2024-12-14 17:13:31 +0530

    fixed db

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
 mongoose
-  .connect("mongodb://localhost:27017", {
-    dbName: "link_shortener",
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .then(() => console.log("Database is connected"))
+  .then(() => console.log("Database connected"))
   .catch((err) => console.log(err));
+
 
   app.get("/", (req, res) => {
     res.send("Server is working")
